d62a0dbe6d9cd9ceb912c89bc422d6fd6bac60e0
Brandon Hoffman <Precision.bh@gmail.com>
2026-04-30 09:51:57 -0400

    update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Landing from "./pages/Landing"
+import ListingPage from "./components/ListingPage" // 
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/l/:id" element={<ListingPage />} /> {/* 🔥 ADD THIS */}
       </Routes>
     </BrowserRouter>
   )
